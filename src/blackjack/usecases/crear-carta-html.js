bf3265d53247de19bb6f0de3db19a7789ade14ac
Sadhit <sadhit.vilca.poma@outlook.es>
2026-06-14 20:45:14 -0500

    crear-carta-html updated

--- src/blackjack/usecases/crear-carta-html.js
+++ src/blackjack/usecases/crear-carta-html.js
@@ -9,7 +9,7 @@ export const crearCartaHTML = (carta) => {
 
      // <img class="carta" src="assets/cartas/2C.png">
         const imgCarta = document.createElement('img');
-        imgCarta.src = `/assets/cartas/${ carta }.png`; //3H, JD
+        imgCarta.src = `/js-vite-blackjack/assets/cartas/${ carta }.png`; //3H, JD
         imgCarta.classList.add('carta');
 
         return imgCarta;
